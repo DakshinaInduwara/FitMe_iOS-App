b29f2f1b8cd667545d156a5b1fab388a4717c1d0
DakshinaInduwara <dakshinainduwara15@gmail.com>
2024-10-29 20:52:33 +0530

    initialized project

---
+++ FitMe/FitMe/FitMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitMeApp.swift
+//  FitMe
+//
+//  Created by Gairuka Bandara on 2024-10-29.
+//
+
+import SwiftUI
+
+@main
+struct FitMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
