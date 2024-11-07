10934f4d8676d0803fa0d90f511e0f900958330b
DakshinaInduwara <dakshinainduwara15@gmail.com>
2024-11-07 20:04:42 +0530

    Add ChartView

---
+++ FitMe/FitMe/Charts/Views/ChartsView.swift
@@ -0,0 +1,58 @@
+//
+//  ChartsView.swift
+//  FitMe
+//
+//  Created by Gairuka Bandara on 2024-11-07.
+//
+
+import SwiftUI
+import Charts
+
+struct DailyStepModel: Identifiable {
+    let id = UUID()
+    let date: Date
+    let count: Double
+}
+
+enum ChartOptions {
+    
+}
+
+class ChartViewModel: ObservableObject {
+    var mockChartData = [
+        DailyStepModel(date: Date(), count: 12345),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date(), count: 99975),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -2, to: Date()) ?? Date(), count: 99975),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -3, to: Date()) ?? Date(), count: 99975),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -4, to: Date()) ?? Date(), count: 99975),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -5, to: Date()) ?? Date(), count: 99975),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -6, to: Date()) ?? Date(), count: 99975),
+    ]
+}
+
+struct ChartsView: View {
+    @StateObject var viewModel = ChartViewModel()
+    var body: some View {
+        VStack{
+            Text("Charts")
+                .font(.largeTitle)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            
+            Chart {
+                ForEach(viewModel.mockChartData) { data in
+                    BarMark(x: .value(data.date.formatted(), data.date, unit: .day), y: .value("Steps",data.count))
+                }
+            }
+            .foregroundColor(.green)
+            .frame(maxHeight: 350)
+            .padding(.horizontal)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+    }
+}
+
+#Preview {
+    ChartsView()
+}
