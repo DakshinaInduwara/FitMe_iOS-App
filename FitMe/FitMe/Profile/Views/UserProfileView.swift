4f1f87e9b1559c3fb41cbaa5c018398a578962e9
DakshinaInduwara <dakshinainduwara15@gmail.com>
2024-11-11 15:57:37 +0530

    Add chnges for UserProfileView

--- FitMe/FitMe/Profile/Views/UserProfileView.swift
+++ FitMe/FitMe/Profile/Views/UserProfileView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct UserProfileView: View {
     var body: some View {
         VStack{
-            HStack{
+            HStack(spacing: 16){
                 Image("man")
                     .resizable()
                     .scaledToFit()
@@ -18,9 +18,48 @@ struct UserProfileView: View {
                     .padding(.all, 8)
                     .background(
                         RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(.gray.opacity(0.4))
+                            .foregroundColor(.gray.opacity(0.25))
                     )
+                VStack(alignment: .leading) {
+                    Text("Good morning")
+                        .font(.largeTitle)
+                        .foregroundColor(.gray)
+                    Text("Name")
+                        .font(.title)
+                }
             }
+            
+            VStack{
+                FitMeProfileButton(title: "Edit Name", image: "square.and.pencil"){
+                    print("name")
+                }
+                
+                FitMeProfileButton(title: "Edit Image", image: "square.and.pencil"){
+                    print("image")
+                }
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.gray.opacity(0.15))
+            )
+            
+            VStack{
+                FitMeProfileButton(title: "Contact Us", image: "envelope"){
+                    print("contact")
+                }
+                
+                FitMeProfileButton(title: "Privacy Policy", image: "document"){
+                    print("privacy")
+                }
+                FitMeProfileButton(title: "Terms of Services", image: "document.badge.arrow.up"){
+                    print("terms")
+                }
+                
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.gray.opacity(0.15))
+            )
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
